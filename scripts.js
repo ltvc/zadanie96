6c6c92e2b8bd7ab94c6e7d1e3fc9366d5b9a7de0
ltvc <lataviec666@gmail.com>
2018-05-22 20:42:33 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+console.log(window);
+console.log(document);
+klawisz.addEventListener("click", function(){
+document.getElementById('addElem');
+var element = document.createElement('li');
+list.appendChild(element);}
+var list = document.getElementById('list');
+var add= document.getElementById('addElem');
+element.innerHTML = 'item';
+var i= (li.getElementsByTagName);
+var j=i.lenght;
+
