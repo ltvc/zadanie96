48b5fd97e56a7e0cd82e0f94f774e780137999e6
ltvc <38085145+ltvc@users.noreply.github.com>
2018-05-24 18:18:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 console.log(window);
 console.log(document);
+var klawisz;
 klawisz.addEventListener("click", function(){
 document.getElementById('addElem');
 var element = document.createElement('li');
-list.appendChild(element);}
+list.appendChild(element);
+});
 var list = document.getElementById('list');
 var add= document.getElementById('addElem');
 element.innerHTML = 'item';
 var i= (li.getElementsByTagName);
 var j=i.lenght;
-
