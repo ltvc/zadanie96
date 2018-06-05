81219e0607565558682f6afd17a014102292cdeb
ltvc <38085145+ltvc@users.noreply.github.com>
2018-06-05 20:42:03 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,8 +6,11 @@ add.addEventListener("click",
 function(){
 document.getElementById('addElem');
 var element = document.createElement('li');
-var numer = document.getElementsByTagName("li");
-var dodajNumer=numer.length;
+//var numer = document.getElementsByTagName("list");
+//var dodajNumer=document.getElementsByName("item").length;
+//var dodajNumer=document.getElementById('list').length;
+var dodajNumer = list.length;
+console.log(dodajNumer);
 element.innerHTML = 'item '+dodajNumer;
 list.appendChild(element);
 });
